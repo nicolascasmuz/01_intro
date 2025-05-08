59483dbd92213c661199b57243fae9d9866a6892
nicolascasmuz <nicolascasmuz@hotmail.com>
2025-05-08 14:18:15 -0300

    commit-001

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,16 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { ImageBackground, StyleSheet, Text, View } from "react-native";
+
+import FitzRoyMountImg from "@/assets/images/monte-fitz-roy.jpg";
 
 const app = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hola mundo</Text>
+      <ImageBackground style={styles.img} source={FitzRoyMountImg}>
+        <View style={styles.overlay}>
+          <Text style={styles.text}>Hola mundo</Text>
+        </View>
+      </ImageBackground>
     </View>
   );
 };
@@ -14,8 +20,18 @@ export default app;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: "column",
+  },
+  img: {
+    width: "100%",
+    height: "100%",
+    flex: 1,
+    resizeMode: "cover",
+  },
+  overlay: {
+    flex: 1,
     justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#00000090",
   },
   text: {
     color: "white",
